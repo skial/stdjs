881b1ec3ad86f1976aef0972baff5aa71c16391b
postite <blog@postite.com>
2012-10-11 22:05:55 +0200

    switch to localstorage

--- haxelib/js/w3c/webapi/WebStorage.hx
+++ haxelib/js/w3c/webapi/WebStorage.hx
@@ -30,7 +30,7 @@ package js.w3c.webapi;
 import js.w3c.DOMTypes;
 import js.w3c.level3.Events;
 
-@:native("Storage")
+@:native("localStorage")
 extern class Storage  implements ArrayAccess<String> {
 	var length(default,null) : Int;
 	function key( index : Int ) : Null<String>;
